c69c62c6d143f143b7182519d034ee5824786bd7
Eduardo Amaro <eduam96@hotmail.com>
2016-03-03 16:06:48 -0600

    If
    If

---
+++ Práctica.R
@@ -0,0 +1,14 @@
+x <- 63
+if (1<x & x<10){
+  w <- "¿Que pedo we?"
+} else if (11<x & x<30){
+  w <- "¿Que onda?"
+} else if (31<x & x<60){
+  w <- "Hola"
+} else if (61<x & x<80){
+  w <- "¿Que pex?"
+} else if (81<x & x<100){
+  w <- "quiubo"
+}
+w
+x
